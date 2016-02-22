e43ec7e40b660dcb490625e74d08f0c815d94667
c.creusot <c.creusot@meetic-corp.com>
2016-02-22 11:18:18 +0100

    Add implementation

--- app/src/main/kotlin/com.meetic/calabash_template/LoggedActivity.kt
+++ app/src/main/kotlin/com.meetic/calabash_template/LoggedActivity.kt
@@ -1,9 +1,16 @@
 package com.meetic.calabash_template
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
 /**
  * *  Meetic
  * *  Copyright (c) $year. All rights reserved.
  */
-class LoggedActivity : AppCompatActivity()
+class LoggedActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState : Bundle?) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.logged_activity);
+    }
+}
--- app/src/main/kotlin/com.meetic/calabash_template/MainActivity.kt
+++ app/src/main/kotlin/com.meetic/calabash_template/MainActivity.kt
@@ -1,6 +1,30 @@
 package com.meetic.calabash_template
 
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.main_activity.*
+
 /**
  **  Meetic
  **  Copyright (c) $year. All rights reserved.
  */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState : Bundle?) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+
+        loginButton.setOnClickListener {
+            v ->
+            if (emailEditTextView.text.isNotEmpty() && passwordEditTextView.text.isNotEmpty()) {
+                startActivity(Intent(this, LoggedActivity::class.java));
+            } else {
+                emailEditTextView.error = "You should fill the email";
+                passwordEditTextView.error = "You should set a password";
+            }
+        }
+    }
+}
